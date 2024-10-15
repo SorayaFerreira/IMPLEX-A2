41b3eb575a4971967802bf72719336a8b0a249f3
Soraya Dias Ferreira <113856615+SorayaFerreira@users.noreply.github.com>
2024-10-15 15:15:41 -0400

    Inicia implementação da solução em programação dinâmica no arquivo dinamic.py

--- dinamic.py
+++ dinamic.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 # Estudantes: Sophya Ribeiro e Soraya Ferreira
+
+def bottom_up_cut_rod(prices, total_size):
+    r = [0 for _ in range(total_size)]
+    
+    for j in range(total_size):
+        q = float('-inf')
+        for i in range(j):
+            q = max(q, p[i] + r[j-i])
+               
+        r[j] = q
+    return r[total_size]
+
+def main():
+    
+   
+
+if __name__ == '__main__':
+    main()
