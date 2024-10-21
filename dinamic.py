882b6c94b4a4b954e269b6087c023ce04bbe0163
SorayaFerreira <soraya.ferreira@ufms.br>
2024-10-20 23:44:19 -0400

    Implementa a versão dinâmica de solução do problema Rod Cutting.

--- dinamic.py
+++ dinamic.py
@@ -1,20 +1,44 @@
 # -*- coding: utf-8 -*-
 # Estudantes: Sophya Ribeiro e Soraya Ferreira
 
-def bottom_up_cut_rod(prices, total_size):
-    r = [0 for _ in range(total_size)]
-    
-    for j in range(total_size):
-        q = float('-inf')
-        for i in range(j):
-            q = max(q, p[i] + r[j-i])
-               
-        r[j] = q
-    return r[total_size]
+import random
+
+def gerar_numeros_aleatorios(n: int) -> list:
+    """
+    Esta função recebe 'n' como o tamanho do vetor a ser 
+    gerado e retorna uma lista com n números aleatórios, sendo
+    que os valores dentro do vetor não podem ultrapassar o valor de 
+    '2n'.
+    """
+ 
+    # 'n' limitação para manter o tempo do counting sort linear
+    numeros = [random.randint(0, 2*n) for _ in range(n)]
+    return numeros
+
+
+
+def bottom_up_cut_rod(prices: list, total_size: int) -> int:
+
+    storage = [0 for _ in range(0, total_size+1)]
+
+    for i in range(1, total_size + 1):
+        melhor_solu = float('-inf')
+        for j in range(0, i):
+            melhor_solu = max(melhor_solu, prices[j] + storage[i - j - 1])
+        storage[i] = melhor_solu
+
+    return storage[total_size]
+       
 
 def main():
-    
-   
+    print("Tamanho total da tora de madeira: ", end="")
+    valor = int(input())
+
+    #gerando vetor de preços
+    #prices = gerar_numeros_aleatorios(valor)
+    prices = [1,  5,  8,  9, 10, 17, 17, 20, 24, 30]
+
+    print(f"O valor máximo de revenda é R$ {bottom_up_cut_rod(prices, valor)}")  
 
 if __name__ == '__main__':
     main()
