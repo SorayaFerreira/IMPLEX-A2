d48bd58faf3d746942a833fd2b6ec87c1ebda63b
SorayaFerreira <soraya.ferreira@ufms.br>
2024-10-21 02:15:29 -0400

    implementa estratégia gulosa para corte da tora

--- dinamic.py
+++ dinamic.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 # Estudantes: Sophya Ribeiro e Soraya Ferreira
 
+
+
+"""
+PENDÊNCIAS:
+    Ordenar de modo não decrescente a lista de preços;
+    A main() faz um pequeno teste, não testando com a lista de aleatórios;
+    A lista de preços deve ser igual para todos os casos de teste? Ou gero listas diferentes a cada teste?
+"""
+
 import random
 
-def gerar_numeros_aleatorios(n: int) -> list:
+def gerar_precos(n: int) -> list:
     """
     Esta função recebe 'n' como o tamanho do vetor a ser 
     gerado e retorna uma lista com n números aleatórios, sendo
@@ -22,10 +31,10 @@ def bottom_up_cut_rod(prices: list, total_size: int) -> int:
     storage = [0 for _ in range(0, total_size+1)]
 
     for i in range(1, total_size + 1):
-        melhor_solu = float('-inf')
+        best_solu = float('-inf')
         for j in range(0, i):
-            melhor_solu = max(melhor_solu, prices[j] + storage[i - j - 1])
-        storage[i] = melhor_solu
+            best_solu = max(best_solu, prices[j] + storage[i - j - 1])
+        storage[i] = best_solu
 
     return storage[total_size]
        
--- greedy.py
+++ greedy.py
@@ -1,2 +1,43 @@
 # -*- coding: utf-8 -*-
 # Estudantes: Sophya Ribeiro e Soraya Ferreira
+
+def greedy_cut_rod(sizes: list, prices: list) -> int:
+    """
+    Essa estratégia gulosa prioriza as toras com maior densidade.
+    """    
+    # Densidade é a razão entre o preço do tamanho i e o tamanho i.
+
+    best_value = 0
+    total_size = len(sizes)
+
+    while total_size > 0:
+        best_density = 0
+        best_density_i = -1
+
+        for i in range(0, total_size):
+            if sizes[i] <= total_size:
+                density = prices[i] / sizes[i]
+                if density > best_density:
+                    best_density = density
+                    best_density_i = i
+
+        if best_density_i == -1:
+            break
+
+        best_value += prices[best_density_i]
+        total_size -= sizes[best_density_i]
+    return best_value
+
+def main():
+    print("Tamanho total da tora de madeira: ", end="")
+    valor = int(input())
+
+    #gerando vetor de preços
+    #prices = gerar_numeros_aleatorios(valor)
+    precos = [1,  5,  8,  9, 10, 17, 17, 20, 24, 30]
+    tamanhos  = [_ for _  in range(1, valor + 1)]
+
+    print(f"O valor máximo de revenda é R$ {greedy_cut_rod(tamanhos, precos)}")  
+
+if __name__ == '__main__':
+    main()
